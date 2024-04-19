ebf7d6ba81e818eaabf51114b36e5ccc01218901
Jorge Centeno Mesa <eljorgecenteno@gmail.com>
2024-04-19 21:10:42 +0200

    changes

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const fileUploader = require("../config/cloudinary.config");
 
 // ℹ️ Handles password encryption
 const bcrypt = require("bcrypt");
@@ -17,9 +18,10 @@ const { isAuthenticated } = require("../middleware/jwt.middleware.js");
 const saltRounds = 10;
 
 // POST /auth/signup  - Creates a new user in the database
-router.post("/signup", (req, res, next) => {
-  const { email, password, name } = req.body;
+router.post("/persons", (req, res, next) => {
+  const { name, age, profile_image_url, interest, description,password, city, email } = req.body;
 
+  console.log(req.body)
   // Check if email or password or name are provided as empty strings
   if (email === "" || password === "" || name === "") {
     res.status(400).json({ message: "Provide email, password and name" });
@@ -58,7 +60,7 @@ router.post("/signup", (req, res, next) => {
 
       // Create the new user in the database
       // We return a pending promise, which allows us to chain another `then`
-      return Person.create({ email, password: hashedPassword, name });
+      return Person.create({ email, password: hashedPassword, name, age, profile_image_url, interest, description, city });
     })
     .then((createdPerson) => {
       // Deconstruct the newly created user object to omit the password
@@ -66,7 +68,7 @@ router.post("/signup", (req, res, next) => {
       const { email, name, _id } = createdPerson;
 
       // Create a new object that doesn't expose the password
-      const person = { email, name, _id };
+      const person = { email, name, _id, profile_image_url, interest };
 
       // Send a json response containing the user object
       res.status(201).json({ user: person });
